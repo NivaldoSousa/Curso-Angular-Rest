36cf30e3b6d55c31f346abd32ee1e6a3b9571215
Nivaldo Sousa <nillsousa026@gmail.com>
2020-09-04 08:00:19 -0300

    Inserindo e removendo os telefones

--- src/app/componente/usuario/usuario-add/usuario-add.component.ts
+++ src/app/componente/usuario/usuario-add/usuario-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/model/user';
 import { UsuarioService } from 'src/app/service/usuario.service';
+import { Telefone } from 'src/app/model/telefone';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,8 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 export class UsuarioAddComponent implements OnInit {
 
   usuario = new User();
+
+  telefone = new Telefone();
 
   constructor(private routeActive: ActivatedRoute, private userService: UsuarioService) { }
 
@@ -40,7 +43,29 @@ export class UsuarioAddComponent implements OnInit {
     }
   }
 
+  deletarTelefone(id, i) {
+    if (id == null) {
+      this.usuario.telefones.splice(i, 1);
+      return;
+    }
+
+    if (id !== null && confirm("Deseja Remover?")) {
+      this.userService.removerTelefone(id).subscribe(data => {
+        this.usuario.telefones.splice(i, 1);
+      });
+    }
+  }
+
+  addFone() {
+    if (this.usuario.telefones === undefined) {
+      this.usuario.telefones = new Array<Telefone>();
+    }
+    this.usuario.telefones.push(this.telefone);
+    this.telefone = new Telefone();
+  }
+
   novo() {
     this.usuario = new User();
+    this.telefone = new Telefone();
   }
 }
